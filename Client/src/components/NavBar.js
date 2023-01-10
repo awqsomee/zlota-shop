a3c3a24554c8b1389c3def56304a7df44c837bf5
awqsomee <awqsome@bk.ru>
2023-01-10 17:39:19 +0300

    123

--- Client/src/components/NavBar.js
+++ Client/src/components/NavBar.js
@@ -9,47 +9,48 @@ import { observer } from 'mobx-react-lite'
 import Container from 'react-bootstrap/Container'
 import { useNavigate } from 'react-router-dom'
 const NavBar = observer(() => {
-    const { user } = useContext(Context)
-    const navigate = useNavigate()
+  const { user } = useContext(Context)
+  const navigate = useNavigate()
 
-    const logOut = () => {
-        user.setUser({})
-        user.setIsAuth(false)
-    }
+  const logOut = () => {
+    user.setUser({})
+    user.setIsAuth(false)
+    localStorage.removeItem('token')
+  }
 
-    return (
-        <Navbar bg="dark" variant="dark">
-            <Container>
-                <NavLink style={{ color: 'white' }} to={SHOP_ROUTE}>
-                    С иголочки
-                </NavLink>
-                {console.log(user)}
-                {user.isAuth ? (
-                    <Nav className="ml-auto" style={{ color: 'white' }}>
-                        {user.user.role === 'ADMIN' ? (
-                            <Button variant={'outline-light'} onClick={() => navigate(ADMIN_ROUTE)}>
-                                Админ панель
-                            </Button>
-                        ) : (
-                            <></>
-                        )}
-                        <Button variant={'outline-light'} onClick={() => navigate(BASKET_ROUTE)}>
-                            Корзина
-                        </Button>
-                        <Button variant={'outline-light'} onClick={() => logOut()} className="ml-2">
-                            Выйти
-                        </Button>
-                    </Nav>
-                ) : (
-                    <Nav className="ml-auto" style={{ color: 'white' }}>
-                        <Button variant={'outline-light'} onClick={() => navigate(LOGIN_ROUTE)}>
-                            Авторизация
-                        </Button>
-                    </Nav>
-                )}
-            </Container>
-        </Navbar>
-    )
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <NavLink style={{ color: 'white' }} to={SHOP_ROUTE}>
+          С иголочки
+        </NavLink>
+        {console.log(user)}
+        {user.isAuth ? (
+          <Nav className="ml-auto" style={{ color: 'white' }}>
+            {user.user.role === 'ADMIN' ? (
+              <Button variant={'outline-light'} onClick={() => navigate(ADMIN_ROUTE)}>
+                Админ панель
+              </Button>
+            ) : (
+              <></>
+            )}
+            <Button variant={'outline-light'} onClick={() => navigate(BASKET_ROUTE)}>
+              Корзина
+            </Button>
+            <Button variant={'outline-light'} onClick={() => logOut()} className="ml-2">
+              Выйти
+            </Button>
+          </Nav>
+        ) : (
+          <Nav className="ml-auto" style={{ color: 'white' }}>
+            <Button variant={'outline-light'} onClick={() => navigate(LOGIN_ROUTE)}>
+              Авторизация
+            </Button>
+          </Nav>
+        )}
+      </Container>
+    </Navbar>
+  )
 })
 
 export default NavBar
